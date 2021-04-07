35a7a161b05501315611471940c6b8b15b5f7e0d
Mélodie Mandolfo <m.mandolfo@codeur.online>
2021-04-07 15:28:42 +0200

    newmodifs

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
+// Navbar
 var loader = setInterval(function () {
     if(document.readyState !== "complete") return;
     clearInterval(loader);
-  document.querySelector(".btn-find").addClass("animated bounce");
+  
 }, 300);
 
-document.querySelector('#contact-form').addEventListener('submit', function (e) {
-  e.preventDefault();
-  e.target.elements.name.value = '';
-  e.target.elements.email.value = '';
-  e.target.elements.message.value = '';
-});
-
-
+// Header
 VANTA.BIRDS({
   el: "#birds",
   mouseControls: true,
@@ -21,7 +15,28 @@ VANTA.BIRDS({
   minWidth: 200.00,
   scale: 1.00,
   scaleMobile: 1.00,
-  backgroundColor: 0x0,
   color1: 0xdaa520,
   color2: 0x0
+});
+
+// Skill bar
+var skillbars = document.querySelectorAll('.skillbar-bar');
+var offsetTop = document.querySelector('#skills').offsetTop;
+window.addEventListener("scroll", function(){ 
+var height = window.innerHeight;
+console.log(window.scrollTop);
+if(window.pageYOffset+height > offsetTop) {
+  for (var i = 0; i < skillbars.length; i++) {
+    console.log(skillbars[i].parentNode.dataset.percent);
+    skillbars[i].style.width = skillbars[i].parentNode.dataset.percent;
+  }
+}
+});
+
+// Footer
+document.querySelector('#contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
 });
