9af4acc839a75603af6fbef2cf35e3b22f484d6a
Mélodie Mandolfo <m.mandolfo@codeur.online>
2021-04-21 14:08:07 +0200

    ajouts

--- main.js
+++ main.js
@@ -35,3 +35,27 @@ document.querySelector('#contact-form').addEventListener('submit', function (e) 
   e.target.elements.email.value = '';
   e.target.elements.message.value = '';
 });
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
